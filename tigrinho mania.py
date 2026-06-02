762ec09e10f76b762172513cf085c7ae1edd0bcf
Marco Vitto <marcosouzavitto11@gmail.com>
2026-06-02 14:10:59 -0300

    Enhance user login, registration, and deposit features
    Refactor login and registration functions to include email_user variable. Add deposit functionality with buttons for different amounts.

--- tigrinho mania.py
+++ tigrinho mania.py
@@ -1,5 +1,7 @@
+import random
 import tkinter as tk
 import sqlite3
+
 name_user = "Guest"
 saldo = 0
 janela = tk.Tk()
@@ -34,8 +36,7 @@ def menu():
     # =====~Selo~===========----------
     tk.Label(janela, text="⋈ SELO DE HONESTIDADE DA O.N.U ⋈", bg="#121212", fg="#F5F5F7", font=("Arial", 10, "bold")).place(relx=0.02,rely=0.95)
 
-    #~~~~~~EXCLOI~~~~~~~~~~~~~~~~
-    tk.Button(janela,text="Pular",command=lets_go_gambling).place(relx=0.1,rely=0.9)
+
 
 
 def login():
@@ -43,7 +44,7 @@ def login():
         elementos.destroy()
     janela.config(bg="#403f3e")
     def pegar_dados():
-        global name_user, saldo
+        global name_user, saldo, email_user
         e = email_entry.get().strip()
         p = password_entry.get().strip()
         funcio.execute("SELECT email, password FROM bancoDeDados WHERE email = ? AND password = ?",(e,p))
@@ -56,6 +57,7 @@ def login():
             funcio.execute("SELECT username,saldo from bancoDeDados WHERE email = ?", (e,))
             resultado = funcio.fetchone()
             name_user = resultado[0]
+            email_user = e
             saldo = resultado[1]
             lets_go_gambling()
 
@@ -83,6 +85,7 @@ def login():
 
 
 def cadastro():
+    global email_user
     for elementos in janela.winfo_children():
         elementos.destroy()
     janela.config(bg="#403f3e")
@@ -114,8 +117,10 @@ def cadastro():
             aviso = tk.Label(frame_menu, text="CADASTRO REALIZADO COM SUCESSO!", fg="green", bg="#222626",font=("Arial", 12, "bold"))
             funcio.execute("""INSERT INTO bancoDeDados(username, email, password, saldo)VALUES (?, ?, ?,?)""", (u, e, p, saldo))
             name_user = u
-            menu()
+            email_user = e
             conexao.commit()
+            menu()
+            return
         aviso.place(relx=0.5, rely=0.715, anchor="center")
     # =====~Frame~===========----------
     frame_menu = tk.Frame(janela, bg="#222626")
@@ -149,9 +154,54 @@ def cadastro():
 
     tk.Button(janela,text="Back", command=menu).place(relx=0.04,rely=0.9)
 
+def deposito():
+    global email_user, saldo
+    for elementos in janela.winfo_children():
+        elementos.destroy()
+    janela.config(bg="#403f3e")
+    # =====~Frame~===========----------
+    frame_menu = tk.Frame(janela, bg="#cc1212")
+    frame_menu.place(relx=0.5,rely=0.05,relwidth=1,relheight=0.15,anchor="center")
+    # =====~Principal Menu~===========----------
+    tk.Label(frame_menu, text="Grande\nOnção", bg="#cc1212",fg="white", font=("Agency FB", 24, "bold")).place(relx=0.1,rely=0.55, anchor="center")
+    ola = tk.Label(frame_menu,text=f"Hello, {name_user} 🐯!",bg="#cc1212",fg="white", font=("Arial", 10, "bold"))
+    ola.place(relx=0.75,rely=0.3)
+    saldo_labeldep = tk.Label(frame_menu,text=f"R$ {saldo:.2f}",bg="#cc1212", fg= "gold", font=("Arial", 15))
+    saldo_labeldep.place(relx=0.75,rely=0.5)
+    # =====~Comando~===========----------
+    def add10():
+        global saldo
+        saldo += 10
+        saldo_labeldep.config(text=f"R$ {saldo:.2f}")
+        funcio.execute("UPDATE bancoDeDados SET saldo = ? WHERE email = ?",(saldo, email_user))
+        conexao.commit()
+    def add50():
+        global saldo
+        saldo += 50
+        saldo_labeldep.config(text=f"R$ {saldo:.2f}")
+        funcio.execute("UPDATE bancoDeDados SET saldo = ? WHERE email = ?",(saldo, email_user))
+        conexao.commit()
+
+    def add100():
+        global saldo
+        saldo += 100
+        saldo_labeldep.config(text=f"R$ {saldo:.2f}")
+        funcio.execute("UPDATE bancoDeDados SET saldo = ? WHERE email = ?",(saldo, email_user))
+        conexao.commit()
+
+
+    ten = tk.Button(janela, text="R$ 10,00", command=add10)
+    ten.place(relx=0.4,rely=0.5,anchor="center")
+    fifty = tk.Button(janela, text="R$ 50,00", command=add50)
+    fifty.place(relx=0.5,rely=0.5,anchor="center")
+    hundred = tk.Button(janela, text="R$ 100,00", command=add100)
+    hundred.place(relx=0.6,rely=0.5,anchor="center")
+    tk.Button(janela, text="Voltar para o Jogo", command=lets_go_gambling, bg="white").place(relx=0.05, rely=0.9)
+
 
 def lets_go_gambling():
-    global name_user, saldo
+    global name_user, saldo,deposito, valor
+    valor = 0.0
     for elementos in janela.winfo_children():
         elementos.destroy()
     janela.config(bg="#403f3e")
@@ -164,8 +214,99 @@ def lets_go_gambling():
     ola.place(relx=0.75,rely=0.3)
     saldo_label = tk.Label(frame_menu,text=f"R$ {saldo:.2f}",bg="#cc1212", fg= "gold", font=("Arial", 15))
     saldo_label.place(relx=0.75,rely=0.5)
-    deposito = tk.Button(frame_menu, text="Depósito", fg="black", bg="lightyellow",font=("Arial",12))
-    deposito.place(relx=0.9,rely=0.5)
+    botaodeposito = tk.Button(frame_menu, text="Depósito", fg="black", bg="lightyellow",font=("Arial",12),command=deposito)
+    botaodeposito.place(relx=0.9,rely=0.5)
+    resultado = 0
+    # =====~Jogo ~===========----------
+    emojis = ["🐯","🐉","🐙"]
+    numbers = []
+    randomA = "⬛"
+    randomB = "⬛"
+    randomC = "⬛"
+    visor = tk.Frame(janela, bg="#262626", highlightthickness= 2, highlightcolor="white")
+    visor.place(relx=0.5, rely=0.45,relwidth=0.4,relheight=0.12,anchor="center")
+    def aposta():
+        global randomA, randomB, randomC, saldo, texto, valor
+        if valor <= saldo and valor > 0:
+            saldo -= valor
+            saldo_label.config(text=f"R$ {saldo:.2f}")
+            numbers.clear()
+            cont = 0
+            for i in range(3):
+                num = random.randint(1,3)
+                numbers.append(num)
+            for i in numbers:
+                cont += 1
+                if i == 1:
+                    if cont == 1:
+                        randomA = "🐯"
+                        p1.config(text=randomA, fg="orange")
+                    if cont == 2:
+                        randomB = "🐯"
+                        p2.config(text=randomB, fg="orange")
+                    if cont == 3:
+                        randomC = "🐯"
+                        p3.config(text=randomC, fg="orange")
+                if i == 2:
+                    if cont == 1:
+                        randomA = "🐉"
+                        p1.config(text=randomA, fg="green")
+                    if cont == 2:
+                        randomB = "🐉"
+                        p2.config(text=randomB, fg="green")
+                    if cont == 3:
+                        randomC = "🐉"
+                        p3.config(text=randomC, fg="green")
+                if i == 3:
+                    if cont == 1:
+                        randomA = "🐙"
+                        p1.config(text=randomA, fg="red")
+                    if cont == 2:
+                        randomB = "🐙"
+                        p2.config(text=randomB, fg="red")
+                    if cont == 3:
+                        randomC = "🐙"
+                        p3.config(text=randomC, fg="red")
+            if numbers[0] == numbers[1] == numbers[2]:
+                saldo += (valor*4)
+                saldo_label.config(text=f"R$ {saldo:.2f}")
+    p1 = tk.Label(janela, text=f"{randomA}", bg="#262626", font=("Arial", 24))
+    p2 = tk.Label(janela, text=f"{randomB}", bg="#262626", font=("Arial", 24))
+    p3 = tk.Label(janela, text=f"{randomC}", bg="#262626", font=("Arial", 24))
+    p1.place(relx=0.4, rely=0.45, anchor="center")
+    p2.place(relx=0.5, rely=0.45,anchor="center")
+    p3.place(relx=0.6, rely=0.45,anchor="center")
+
+    botao = tk.Button(janela, text="ROLL", bg="#12E06A", command=aposta)
+    botao.place(relx=0.5,rely=0.75,relwidth=0.2,relheight=0.08,anchor="center")
+
+    def validar(texto):
+        if texto == "" or texto == ".":
+            return True
+        if texto.replace(".", "", 1).isdigit():
+            if "." in texto:
+                return len(texto.split(".")[1]) <= 2
+            return True
+        return False
+    validacao = janela.register(validar)
+    def lancar_valor():
+        global valor, texto
+        texto = valor_aposta.get()
+        if texto and texto != ".":
+            valor = float(texto)
+            if valor <= saldo:
+                valor_selecionado.config(text=f"R$ {valor:.2f}")
+    valor_aposta = tk.Entry(janela,validate="key", validatecommand=(validacao, "%P"), bg="white", highlightcolor="black", highlightthickness=0.2, font=("Arial", 20))
+    valor_aposta.place(relx=0.5,rely=0.6,anchor="center")
+
+    tk.Label(janela,text="Insira valor",bg="#403f3e", fg="white", font=("Arial", 12)).place(relx=0.5,rely=0.55,anchor="center")
+
+    send_aposta = tk.Button(janela, text="➡️", bg="white", activebackground="#dee1e3", command=lancar_valor)
+    send_aposta.place(relx=0.6455,rely=0.6,anchor="center")
+
+    valor_selecionado = tk.Label(janela,text="R$ 0.00",font=("Arial",20,"bold"))
+    valor_selecionado.place(relx=0.5,rely=0.3,anchor="center")
+
 menu()
 janela.mainloop()
 conexao.close()
